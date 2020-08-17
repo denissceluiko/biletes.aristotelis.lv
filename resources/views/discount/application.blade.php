2f82f40cdbe85250577b8d08f6904450505ca3fd
Deniss Celuiko <deniss.celuiko@gmail.com>
2020-08-18 02:18:53 +0300

    Minor style changes

--- resources/views/discount/application.blade.php
+++ resources/views/discount/application.blade.php
@@ -3,9 +3,13 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-6">
+        <div class="col-12 col-lg-6 offset-lg-3">
+            <h1>Atlaide Aristotelim LU studējošajiem</h1>
+            <p>Latvijas Universitātes studentiem ir iespēja iepriekšpārdošanā iegādāties biļeti Aristoteļa svētku vakara daļai par 7€. Atlaižu kodu vari saņemt uz savu <code>@student.lu.lv</code> vai <code>@lu.lv</code> e-pastu, aizpildot anketu zemāk.</p>
+            <p>Vairāk info par studentu svētkiem meklē <a href="https://aristotelis.lv">aristotelis.lv</a>.</p>
             <div class="card">
                 <div class="card-body">
+                    <h5 class="card-title">Saņem savu atlaides kodu</h5>
                     {{ Form::open(['action' => 'DiscountController@apply']) }}
                     <div class="form-group">
                         {{ Form::label('email', 'E-pasts', ['class' => '']) }}
@@ -14,10 +18,11 @@
                             <small class="form-text text-danger">{{ $errors->first('email') }}</small>
                         @endif
                     </div>
-                    {{ Form::submit('Pieteikties', ['class' => 'form-control btn btn-primary']) }}
+                    {{ Form::submit('Saņemt', ['class' => 'form-control btn btn-primary']) }}
                     {{ Form::close() }}
                 </div>
             </div>
+            <p>Neskaidrību gadījumā droši raksti uz <a href="mailto:aristotelis@lusp.lv">aristotelis@lusp.lv</a> </p>
         </div>
     </div>
 </div>
--- resources/views/discount/apply.blade.php
+++ resources/views/discount/apply.blade.php
@@ -3,12 +3,17 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-6">
+            <div class="col-12 col-lg-6 offset-lg-3">
+                <h1>Atlaide Aristotelim LU studējošajiem</h1>
+                <p>Latvijas Universitātes studentiem ir iespēja iepriekšpārdošanā iegādāties biļeti Aristoteļa svētku vakara daļai par 7€. Atlaižu kodu vari saņemt uz savu <code>@student.lu.lv</code> vai <code>@lu.lv</code> e-pastu, aizpildot anketu zemāk.</p>
+                <p>Vairāk info par studentu svētkiem meklē <a href="https://aristotelis.lv">aristotelis.lv</a>.</p>
                 <div class="card">
                     <div class="card-body">
-                        Ataides kods aizsūtīts uz {{ request('email') }}
+                        <h5 class="card-title">Pārbaudi savu e-pastu</h5>
+                        <p> Tavs ataides kods aizsūtīts uz <code>{{ request('email') }}</code></p>
                     </div>
                 </div>
+                <p>Neskaidrību gadījumā droši raksti uz <a href="mailto:aristotelis@lusp.lv">aristotelis@lusp.lv</a> </p>
             </div>
         </div>
     </div>
