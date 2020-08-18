65bac211316069b4ccef4eeb172c65e12344179e
Deniss Celuiko <deniss.celuiko@gmail.com>
2020-08-18 14:02:43 +0300

    Fixed typo

--- resources/views/discount/apply.blade.php
+++ resources/views/discount/apply.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-12 col-lg-6 offset-lg-3">
                 <h1>Atlaide Aristotelim LU studējošajiem</h1>
-                <p>Latvijas Universitātes studentiem ir iespēja iepriekšpārdošanā iegādāties biļeti Aristoteļa svētku vakara daļai par 7€. Atlaižu kodu vari saņemt uz savu <code>@student.lu.lv</code> e-pastu, aizpildot anketu zemāk.</p>
+                <p>Latvijas Universitātes studentiem ir iespēja iepriekšpārdošanā iegādāties biļeti Aristoteļa svētku vakara daļai par 7€. Atlaižu kodu vari saņemt uz savu <code>@students.lu.lv</code> e-pastu, aizpildot anketu zemāk.</p>
                 <p>Vairāk info par studentu svētkiem meklē <a href="https://aristotelis.lv">aristotelis.lv</a>.</p>
                 <div class="card">
                     <div class="card-body">
